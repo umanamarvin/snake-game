1f9176393fb521db9ae4fc3e8dcece2759658d6e
marvin <lio_uma19@hotmail.com>
2024-03-20 13:58:53 +0100

    naming the classes

---
+++ food-class.py
@@ -0,0 +1,18 @@
+from turtle import Turtle, Screen
+import random
+
+
+class Food:
+
+    def __init__(self):
+        pass
+
+    def create(self):
+        pass
+
+    def appear(self):
+        pass
+
+    def collision(self):
+        pass
+
---
+++ snake-class.py
@@ -0,0 +1,19 @@
+from turtle import Turtle, Screen
+import random
+
+
+class Snake:
+    def __init__(self, ):
+        pass
+
+    def create(self):
+        pass
+
+    def move(self):
+        pass
+
+    def extend(self):
+        pass
+
+    def control(self):
+        pass
